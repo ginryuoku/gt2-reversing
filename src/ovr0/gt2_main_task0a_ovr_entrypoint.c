030ae266ae7182f320bdcbeb8f770fee1026e4e2
ginryuoku <36065192+ginryuoku@users.noreply.github.com>
2024-07-27 18:18:00 -0700

    added two more ovr0 functions

---
+++ src/ovr0/gt2_main_task0a_ovr_entrypoint.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+extern void gt2_main_task0a_ovr_func0(int*);                    /* extern */
+extern int D_801EF610;
+
+void gt2_main_task0a_ovr_entrypoint(void) {
+    gt2_main_task0a_ovr_func0(&D_801EF610);
+}
---
+++ src/ovr0/gt2_main_task0a_ovr_func1.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+typedef int s32;
+extern char* D_800A8E5C;// = "gt2.ovl";
+
+extern s32* gt2_main_task082_file_loader();                           /* extern */
+
+s32* gt2_main_task0a_ovr_func1(s32 arg0) {
+    return gt2_main_task082_file_loader(&D_800A8E5C, arg0);
+}
