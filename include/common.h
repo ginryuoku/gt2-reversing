f74b001ef04306b19ead8e26d6615e8426d9e3d7
ginryuoku <36065192+ginryuoku@users.noreply.github.com>
2024-07-31 01:39:16 -0700

    Decompiled vsync handler (NB: not inserted yet)

--- include/common.h
+++ include/common.h
@@ -2,5 +2,6 @@
 #define COMMON_H
 
 #include "asm.h"
+#define NULL 0
 
 #endif /* COMMON_H */
---
+++ src/ovr0/gt2_vsync_handler.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+extern void gt2_main_task0c_gpu_reg1_op(int);                   /* extern */
+extern int D_80011DF4;
+
+void gt2_vsync_handler(void) {
+    gt2_main_task0c_gpu_reg1_op(1);
+    D_80011DF4 += 1;
+}
