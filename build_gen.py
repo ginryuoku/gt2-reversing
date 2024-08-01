fe2faf8f90961567fcc2412f13245d784925b6d6
ginryuoku <36065192+ginryuoku@users.noreply.github.com>
2024-08-01 00:45:51 -0700

    decompiled gt2_main_task0b0_car_loader

--- build_gen.py
+++ build_gen.py
@@ -115,7 +115,8 @@ cpp_targets_autogen_end = ["src/autogen/start.c",
                            "src/autogen/ovr0_3.c",
                            "src/autogen/ovr0_4.c",
                            "src/autogen/ovr0_5.c",
-                           "src/autogen/ovr0_6.c"]
+                           "src/autogen/ovr0_6.c",
+                           "src/autogen/ovr0_7.c"]
 # place any actual c files here
 cpp_targets_end = ["src/ovr0/gt2_main_task0a_ovr_entrypoint.c",
                    "src/ovr0/gt2_main_task0.c",
@@ -123,6 +124,7 @@ cpp_targets_end = ["src/ovr0/gt2_main_task0a_ovr_entrypoint.c",
 #                   "src/ovr0/gt2_sysinit.c",
                    "src/ovr0/gt2_main_task0a_ovr_func1.c",
                    "src/ovr0/gt2_main_task0b.c",
+                   "src/ovr0/gt2_main_task0b0_car_loader.c",
                    "src/ovr0/gt2_main_task0b01_noop_loader.c",
                    "src/ovr0/gt2_unknown_task0.c",
                    "src/ovr0/gt2_vsync_handler.c",
