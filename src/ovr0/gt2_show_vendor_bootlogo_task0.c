74d1f61a25da066dd56d0d1ddc021c5cb4695028
ginryuoku <36065192+ginryuoku@users.noreply.github.com>
2024-08-02 21:33:25 -0700

    Updated ghidra dump (45% mainexe, 17% overall)

--- src/ovr0/gt2_show_vendor_bootlogo_task0.c
+++ src/ovr0/gt2_show_vendor_bootlogo_task0.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
 extern void gt2_callback_double_buffer_flip();                                  /* extern */
-extern void func_80085B3C(int*, char*, int*);                      /* extern */
+extern void gt2_main_flip_double_buffers(int *, char *, int *); /* extern */
 extern char D_80011D84;
 extern int D_801697D0;
 extern int D_801F0CE0;
@@ -9,7 +9,7 @@ extern int D_801F0CE0;
 int* gt2_show_vendor_bootlogo_task0(void) {
     if (D_801697D0 == 0) {
         gt2_callback_double_buffer_flip();
-        func_80085B3C(&D_801697D0, &D_80011D84, &D_801F0CE0);
+        gt2_main_flip_double_buffers(&D_801697D0, &D_80011D84, &D_801F0CE0);
     }
     return &D_801697D0;
 }
