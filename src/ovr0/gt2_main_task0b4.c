839c1926c742952b8d28fedea86d64a023ab6cf9
Michelle Darcy <mdarcy137@gmail.com>
2024-08-07 10:46:48 -0700

    one weird trick to write dummy structs!
    real programmers hate her! (20/1171 decomped mainexe symbols)

---
+++ src/ovr0/gt2_main_task0b4.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+extern int gt2_sysinit_task00(char*, int, int);
+
+typedef struct {
+    short unk0;
+    short unk2;
+} unkstruct_800A6F18;
+
+extern unkstruct_800A6F18 D_800A6F18;
+extern char D_801C98A0;
+
+void gt2_main_task0b4(void) {
+    gt2_sysinit_task00(&D_801C98A0, 0, 0x40);
+    D_800A6F18.unk0 = -0x40;
+    D_800A6F18.unk2 = 0x40;
+}
