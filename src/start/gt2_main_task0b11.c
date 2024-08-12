8231b349270e0372d6e85483f7ca3dff9d23a855
Michelle Darcy <mdarcy137@gmail.com>
2024-08-12 04:47:32 -0700

    gt2_main_task0b11 decomped

---
+++ src/start/gt2_main_task0b11.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+void gt2_main_task0b110(unsigned short);                          /* extern */
+extern unsigned short D_801E2EF0;
+
+void gt2_main_task0b11(unsigned int arg0) {
+    gt2_main_task0b110(*((arg0) + &D_801E2EF0));
+}
