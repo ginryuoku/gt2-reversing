8a22092920b5bccdfd8ff128656d73a803025df9
Michelle Darcy <mdarcy137@gmail.com>
2024-08-12 04:26:09 -0700

    decomped gt2_main_shared_racegtarcade_func6

---
+++ src/start/gt2_main_shared_racegtarcade_func6.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+extern unsigned int gt2_main_shared_racegtarcade_func60;
+
+void gt2_main_task0b1100 (unsigned int, unsigned int, unsigned int *); /* extern */
+extern unsigned int gt2_main_cd_read;
+
+void gt2_main_shared_racegtarcade_func6(unsigned int arg0, unsigned int arg1) {
+    gt2_main_task0b1100(arg0, arg1, &gt2_main_shared_racegtarcade_func60);
+}
