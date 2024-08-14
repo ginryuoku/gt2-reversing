2d33a737440b82c9cabc01c78d9a23bf5458f91d
Michelle Darcy <mdarcy137@gmail.com>
2024-08-14 01:39:16 -0700

    decomped gt2_main_race_func2
    This one matches with both, but turned out we were missing some CFLAGS that PSY-Q normally uses by default. Updated CFLAGS for both 2.8.1 and 2.95.2 to make everything work.

---
+++ src/start/gt2_main_race_func2.c
@@ -0,0 +1,24 @@
+#include "common.h"
+
+// TODO: identify real struct and edit accordingly
+typedef struct {
+    int pad[0x16];
+    int unk58;
+    int unk5C;
+} arg0_race_func2;
+
+void gt2_main_race_func2(arg0_race_func2* arg0, int arg1) {
+    int temp_v0_2;
+    unsigned int temp_v0;
+
+    temp_v0 = arg0->unk5C + arg1;
+    arg0->unk5C = temp_v0;
+    if (temp_v0 > 0x05F5E100U) {
+        temp_v0_2 = arg0->unk58 + 1;
+        arg0->unk58 = temp_v0_2;
+        if (temp_v0_2 < 0) {
+            arg0->unk58 = 0x7FFFFFFF;
+        }
+        arg0->unk5C = (unsigned int) (arg0->unk5C - 0x05F5E100);
+    }
+}
