56a1dc85910f9545c496554af54b2f66eacf0c31
ginryuoku <36065192+ginryuoku@users.noreply.github.com>
2024-09-12 21:18:09 -0700

    added two trivials (possible C++ functions?)

---
+++ src/start/gt2_main_sharedovr_func_16.c
@@ -0,0 +1,6 @@
+// NB: *may* be a C++ class method
+#include "common.h"
+
+void gt2_main_sharedovr_func_16(s8 arg0[]) {
+    arg0[32] = 0;
+}
---
+++ src/start/gt2_main_task_083_file_loader0.c
@@ -0,0 +1,10 @@
+// builds only on 2.8.1
+#include "common.h"
+
+extern s32 D_80095ABC;
+extern s32 D_80095AC0;
+
+void gt2_main_task_083_file_loader0(s32 arg0, s32 arg1) {
+    D_80095ABC = arg0;
+    D_80095AC0 = arg1;
+}
